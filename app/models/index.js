30bbefc6ad043032a57c893a4480b204c20079ed
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-04-28 05:52:10 +0300

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -1,15 +1,19 @@
+const { Sequelize } = require('sequelize');
 const dbConfig = require("../config/db.config.js");
-const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
+  port: dbConfig.PORT,  // Portu da belirtmeyi unutma
+  dialectOptions: {
+    ssl: dbConfig.dialectOptions.ssl
+  },
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
     idle: dbConfig.pool.idle,
-  },
+  }
 });
 
 const db = {};
@@ -18,9 +22,13 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+module.exports = db;
+
+
+// Modelleri içe aktarıyoruz
+
 db.Blog = require("./blog.model.js")(sequelize, Sequelize);
 db.Subscriber = require("./subscriber.model.js")(sequelize, Sequelize);
-db.Statistics = require("./statistics.model.js")(sequelize, Sequelize); // Statistics modelini ekledik
-
+db.Statistics = require("./statistics.model.js")(sequelize, Sequelize); 
 
 module.exports = db;
