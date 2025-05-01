31682241ea9e86a54dd4579e6a7b73ea66e6937f
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-05-02 00:07:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,42 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
+const socketIo = require("socket.io"); // Socket.IO'yu import et
+
 require('dotenv').config();
 
 const app = express();
-// HTTP server'ı başlatıyoruz
 const server = http.createServer(app);
 
+// Socket.IO'yu başlat
+const io = socketIo(server, {
+  cors: {
+    origin: ["http://localhost:3000", "https://aliardagul-av-tr.netlify.app"],
+    credentials: true
+  }
+});
+
+app.set("io", io); // io'yu app'e set et
+
 var corsOptions = {
-  origin: ["http://localhost:3000","https://aliardagul-av-tr.netlify.app"],
+  origin: ["http://localhost:3000", "https://aliardagul-av-tr.netlify.app"],
   credentials: true
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 const db = require("./app/models");
 db.sequelize.sync({ alter: true }).then(() => {
   console.log("Veritabanı senkronize edildi.");
 });
+
 require("./app/routes/blog.routes")(app);
 require("./app/routes/subscriber.routes")(app);
 require("./app/routes/appointment.routes")(app);
 require("./app/routes/statistics.routes")(app);
+
 app.get("/", (req, res) => {
   res.send("hukuk!");
 });
