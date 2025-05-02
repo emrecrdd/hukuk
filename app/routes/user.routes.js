b0936a4bbebf675a24f8a514cb6ef7519131909a
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-05-03 02:35:46 +0300

    Update user.routes.js

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,1 +1,26 @@
+module.exports = app => {
+    const users = require("../controllers/user.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Kullanıcı kaydı (Register)
+    router.post("/register", users.create);
+   
+    // Kullanıcı girişi (Login)
+    router.post("/login", users.login); 
+  
+   // Kullanıcı Profili Alma (GET /api/users/:id)
+    router.get("/:id", users.getUserProfile);
+     // Kullanıcı Bilgilerini Güncelleme (PUT /api/users/:id)
+     router.put("/:id", users.updateUserProfile);
+     // Kullanıcılar Listesi
+    router.get("/", users.getUsers);
+// Şifremi unuttum (şifre sıfırlama isteği)
 
+
+
+
+    // /api/users yoluna yönlendir
+    app.use("/api/users", router);
+  };
+  
